5381fb2b2d82d6df9be8763386808e967fd03085
iamch15542 <kuso1112.cs06@nctu.edu.tw>
2020-12-03 20:32:45 +0800

    finish 1.2.4

--- HW4/pi_gather.cc
+++ HW4/pi_gather.cc
@@ -4,18 +4,13 @@
 #include <time.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <random>       // for random_device
 
-typedef long long int ll;
-
-unsigned int monte_carlo_pi(unsigned int seed, long long per_p) {
-    double x, y;
-    for(ll i = 0; i < per_p; ++i) {
-        x = (double)rand_r(&seed) / RAND_MAX;
-        y = (double)rand_r(&seed) / RAND_MAX;
-        if ((x * x + y * y) <= 1.0) {
-            cnt++;
-        }
-    }
+unsigned int xorshift32(unsigned int& x) {
+    x ^= x << 13;
+    x ^= x >> 17;
+    x ^= x << 5;
+    return x;
 }
 
 int main(int argc, char **argv)
@@ -32,11 +27,29 @@ int main(int argc, char **argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    // TODO: use MPI_Gather
+    // init variable
+    long per_p = tosses / world_size, cnt = 0, recv[world_size];
 
-    if (world_rank == 0)
-    {
+    // run monte carlo
+    std::random_device rd;
+    unsigned int rdn = rd();
+    long long check = 1073741824;
+    for(size_t i = 0; i < per_p; ++i) {
+        unsigned int seed = xorshift32(rdn);
+        unsigned int x = (seed & 0x7FFF0000) >> 16;
+        unsigned int y = (seed & 0x00007FFF);
+        if ((x * x + y * y) <= check) {
+            cnt++;
+        }
+    }
+
+    // TODO: use MPI_Gather
+    MPI_Gather(&cnt, 1, MPI_LONG, recv, 1, MPI_LONG, 0, MPI_COMM_WORLD);
+
+    if (world_rank == 0) {
         // TODO: PI result
+        for(int i = 1; i < world_size; ++i) cnt += recv[i];
+        pi_result = 4 * ((double)cnt / (double)tosses);
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
