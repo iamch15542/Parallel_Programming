ba5cf92df73095b2f59de6cb07ee3e6ca4722d8e
iamch15542 <kuso1112.cs06@nctu.edu.tw>
2020-12-03 22:31:58 +0800

    finish 1.2.6

--- HW4/pi_one_side.cc
+++ HW4/pi_one_side.cc
@@ -4,6 +4,14 @@
 #include <time.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <random>       // for random_device
+
+unsigned int xorshift32(unsigned int& x) {
+    x ^= x << 13;
+    x ^= x >> 17;
+    x ^= x << 5;
+    return x;
+}
 
 int main(int argc, char **argv)
 {
@@ -18,21 +26,45 @@ int main(int argc, char **argv)
     MPI_Win win;
 
     // TODO: MPI init
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    if (world_rank == 0)
-    {
-        // Master
+    // init variable
+    long per_p = tosses / world_size, cnt = 0;
+    long *recv = (long*) malloc(world_size * sizeof(long));
+    MPI_Win_create(recv, world_size * sizeof(long), sizeof(long), MPI_INFO_NULL,MPI_COMM_WORLD, &win);
+
+    // run monte carlo
+    std::random_device rd;
+    unsigned int rdn = rd();
+    long long check = 1073741824;
+    for(size_t i = 0; i < per_p; ++i) {
+        unsigned int seed = xorshift32(rdn);
+        unsigned int x = (seed & 0x7FFF0000) >> 16;
+        unsigned int y = (seed & 0x00007FFF);
+        if ((x * x + y * y) <= check) {
+            cnt++;
+        }
     }
-    else
-    {
+
+    if(world_rank == 0) {
+        // Master
+        MPI_Win_fence(0, win);
+        MPI_Put(&cnt, 1, MPI_LONG, 0, world_rank, 1, MPI_LONG, win);
+        MPI_Win_fence(0, win);
+    } else {
         // Workers
+        MPI_Win_fence(0, win);
+        MPI_Put(&cnt, 1, MPI_LONG, 0, world_rank, 1, MPI_LONG, win);
+        MPI_Win_fence(0, win);
     }
 
     MPI_Win_free(&win);
 
-    if (world_rank == 0)
-    {
+    if(world_rank == 0) {
         // TODO: handle PI result
+        for(int i = 1; i < world_size; ++i) cnt += recv[i];
+        pi_result = 4 * ((double)cnt / (double)tosses);
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
