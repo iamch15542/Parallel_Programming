b00febd68ea26e889c054b2e6c0f7085209528b9
iamch15542 <kuso1112.cs06@nctu.edu.tw>
2020-12-18 01:30:42 +0800

    try method4

--- HW5/kernel.cu
+++ HW5/kernel.cu
@@ -2,29 +2,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-__global__ void mandelKernel(float stepX, float stepY, float lowerX, float lowerY, int* img_result, int maxIterations, int pitch, int groups) {
+__global__ void mandelKernel(float stepX, float stepY, float lowerX, float lowerY, int* img_result, int maxIterations) {
     // To avoid error caused by the floating number, use the following pseudo code
     //
     // float x = lowerX + thisX * stepX;
     // float y = lowerY + thisY * stepY;
 
-    for(int cnt = 0; cnt < groups; ++cnt) {
-        float x = lowerX + ((blockIdx.x * blockDim.x + threadIdx.x) * groups + cnt) * stepX;
-        float y = lowerY + (blockIdx.y * blockDim.y + threadIdx.y) * stepY;
-        
-        float z_re = x, z_im = y;
-        int i;
-        for (i = 0; i < maxIterations; ++i) {
-            if (z_re * z_re + z_im * z_im > 4.f)
-                break;
+    __shared__ int block_result[256];
+    
+    float x = lowerX + (blockIdx.x * blockDim.x + threadIdx.x) * stepX;
+    float y = lowerY + (blockIdx.y * blockDim.y + threadIdx.y) * stepY;
+    
+    float z_re = x, z_im = y;
+    int i;
+    for (i = 0; i < maxIterations; ++i) {
+        if (z_re * z_re + z_im * z_im > 4.f)
+            break;
 
-            float new_re = z_re * z_re - z_im * z_im;
-            float new_im = 2.f * z_re * z_im;
-            z_re = x + new_re;
-            z_im = y + new_im;
+        float new_re = z_re * z_re - z_im * z_im;
+        float new_im = 2.f * z_re * z_im;
+        z_re = x + new_re;
+        z_im = y + new_im;
+    }
+
+    block_result[threadIdx.y * blockDim.x + threadIdx.x] = i;
+    __syncthreads();
+
+    if(threadIdx.x == 0 && threadIdx.y == 0) {
+        for(int i = 0; i < 16; ++i) {
+            for(int j = 0; j < 16; ++j) {
+                int idx = (blockIdx.y * blockDim.y + j) * (gridDim.x * blockDim.x) + (blockIdx.x * blockDim.x + i);
+                img_result[idx] = block_result[j * 16 + i];
+            }
         }
-        int* idx = (int*)((char*)img_result + (blockIdx.y * blockDim.y + threadIdx.y) * pitch) + (blockIdx.x * blockDim.x + threadIdx.x) * groups + cnt;
-        *idx = i;
     }
 }
 
@@ -35,30 +45,20 @@ void hostFE (float upperX, float upperY, float lowerX, float lowerY, int* img, i
     float stepY = (upperY - lowerY) / resY;
 
     // Declare the host memory
-    int *h_result;
-    cudaHostAlloc((void **)&h_result, resX * resY * sizeof(int), cudaHostAllocDefault);
+    // int *h_result = (int *)malloc(resX * resY * sizeof(int));
 
     // Declare the cuda memory
-    int *c_result, groups = 5;
-    size_t pitch;
-    cudaMallocPitch((void **)&c_result, &pitch, sizeof(int) * resX, resY); // 4 * 1600 = 6400 -> pitch = 6656
+    int *c_result;
+    cudaMalloc(&c_result, resX * resY * sizeof(int));
 
     dim3 blockSize(16, 16);
-    dim3 numBlock(resX / 80, resY / 16);
+    dim3 numBlock(resX / 16, resY / 16);
 
-    mandelKernel<<<numBlock, blockSize>>>(stepX, stepY, lowerX, lowerY, c_result, maxIterations, pitch, groups);
-
-    // 等待 GPU 所有 thread 完成
-    cudaDeviceSynchronize();
+    mandelKernel<<<numBlock, blockSize>>>(stepX, stepY, lowerX, lowerY, c_result, maxIterations);
 
     // 將 Device 的資料傳回給 Host
-    cudaMemcpy2D(h_result, resX * sizeof(int), c_result, pitch, resX * sizeof(int), resY, cudaMemcpyDeviceToHost);
-
-    for(int i = 0; i < resX * resY; ++i) {
-        *(img+i) = *(h_result+i);
-    }
+    cudaMemcpy(img, c_result, resX * resY * sizeof(int), cudaMemcpyDeviceToHost);
 
     // free memory
-    cudaFreeHost(h_result);
     cudaFree(c_result);
 }
