2cd29c5bc462bf8752f0b7a4ee8700755855a447
iamch15542 <kuso1112.cs06@nctu.edu.tw>
2021-01-02 14:04:01 +0800

    tmp

--- HW6/hostFE.c
+++ HW6/hostFE.c
@@ -9,5 +9,59 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
 {
     cl_int status;
     int filterSize = filterWidth * filterWidth;
+    int imageSize = imageHeight * imageWidth;
 
+    // create commandqueue
+    cl_command_queue imgqueue;
+    imgqueue = clCreateCommandQueue(*context, *device, 0, &status);
+    CHECK(status, "clCreateCommandQueue");
+
+    // create buffer on device
+    cl_mem d_inputImage = clCreateBuffer(*context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(float) * imageSize, inputImage, &status);
+    cl_mem d_filter = clCreateBuffer(*context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(float) * filterSize, filter, &status);
+    cl_mem d_outputImage = clCreateBuffer(*context, CL_MEM_WRITE_ONLY, sizeof(float) * imageSize, NULL, &status);
+    
+    // cl_mem d_filterWidth = clCreateBuffer(*context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(cl_float), &filterWidth, &status);
+    // cl_mem d_imageHeight = clCreateBuffer(*context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(cl_int), &imageHeight, &status);
+    // cl_mem d_imageWidth = clCreateBuffer(*context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(cl_int), &imageWidth, &status);
+
+    cl_kernel kernel = clCreateKernel(*program, "convolution", &status);
+    CHECK(status, "clCreateKernel");
+    
+    // set kernel Arg
+    clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&d_inputImage);
+    clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&d_filter);
+    clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&d_outputImage);
+    
+    clSetKernelArg(kernel, 3, sizeof(cl_int), (void*)&filterWidth);
+    clSetKernelArg(kernel, 4, sizeof(cl_int), (void*)&imageHeight);
+    clSetKernelArg(kernel, 5, sizeof(cl_int), (void*)&imageWidth);
+    
+    printf("Set Kernel Arg Success\n");
+    
+    // execute kernel
+    size_t localws[2] = {2, 2};
+    size_t globalws[2] = {imageWidth, imageHeight};
+    status = clEnqueueNDRangeKernel(imgqueue, kernel, 2, 0, globalws, localws, 0, NULL, NULL);
+    CHECK(status, "clEnqueueNDRangeKernel");
+
+    printf("Finish kernel\n");
+
+    if(status == CL_SUCCESS) {
+        status = clEnqueueReadBuffer(imgqueue, d_outputImage, CL_TRUE, 0, sizeof(cl_float) * imageSize, outputImage, NULL, NULL, NULL);
+    }
+    CHECK(status, "clEnqueueReadBuffer");
+
+    // release opencl
+    clReleaseKernel(kernel);
+    clReleaseProgram(program);
+    // clReleaseMemObject(d_filterWidth);
+    clReleaseMemObject(d_filter);
+    // clReleaseMemObject(d_imageHeight);
+    // clReleaseMemObject(d_imageWidth);
+    clReleaseMemObject(d_inputImage);
+    clReleaseMemObject(d_outputImage);
+    clReleaseCommandQueue(imgqueue);
+    clReleaseContext(context);
+    return 0;
 }
--- HW6/kernel.cl
+++ HW6/kernel.cl
@@ -1,4 +1,18 @@
-__kernel void convolution() 
+__kernel void convolution(__constant float *inputImage, __constant float *filter, __global float *outputImage,
+                          const int filterWidth, const int imageHeight, const int imageWidth) 
 {
-   
+    // const int i = get_global_id(0); // col
+    // const int j = get_global_id(1); // row
+
+    // int sum = 0;
+    // int halfwidth = filterWidth / 2;
+    // // Apply the filter to the neighborhood
+    // for(int fi = -halfwidth; fi <= halfwidth; ++fi) {
+    //     for(int fj = -halfwidth; fj <= halfwidth; ++fj) {
+    //         if(i + fi >= 0 && i + fi < imageHeight && j + fj >= 0 && j + fj < imageWidth) {
+    //             sum += inputImage[(i + fi) * imageWidth + j + fj] * filter[(fi + halfwidth) * filterWidth + fj + halfwidth];
+    //         }
+    //     }
+    // }
+    // outputImage[i * imageWidth + j] = sum;
 }
